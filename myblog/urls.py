52b5f638374eb3779f0235b7b141755270d21ea2
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-06-24 23:36:58 +0700

    clean code

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-# from personal.views import (
-#     home_screen_view,
-# )
 
 urlpatterns = [
     path('register/', include('account.urls')),
