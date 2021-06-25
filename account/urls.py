d2a6b79881fecbbbd860837c4d5fb792f5cb6555
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-06-25 17:13:58 +0700

    Add bootstrap

--- account/urls.py
+++ account/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.registration_view, name='register'),
+    path('edit/', views.account_view, name='edit'),
     path('logout/', views.logout_view, name='logout'),
 ]
+ 
--- account/views.py
+++ account/views.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib import auth
-from account.form import AccountAuthenticationForm, RegistrationForm
+from account.form import AccountAuthenticationForm, AccountUpdateForm, RegistrationForm
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from account.form import RegistrationForm
@@ -56,3 +56,33 @@ def login_view(request):
 	context['login_form'] = form
 
 	return render(request, 'account/login.html', context)
+
+
+def account_view(request):
+
+    if not request.user.is_authenticated:
+        return redirect('login')
+    
+    context = {}
+
+    if request.method == "POST":
+        form = AccountUpdateForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.initial = {
+					"email": request.POST['email'],
+					"username": request.POST['username'],
+			}
+            form.save()
+            context['success_message'] = "Updated"
+    else:
+        form = AccountUpdateForm(
+            initial={
+					"email": request.user.email, 
+					"username": request.user.username,
+				}
+        )
+    
+    context['account_form'] = form
+    return render(request, 'account/account.html', context)
+        
+
