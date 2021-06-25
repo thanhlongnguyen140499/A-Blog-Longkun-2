55ac6fafccd7ec6cc60c4e6fd3ee999ed77d2df7
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-06-25 23:04:07 +0700

    Add bootstrap, static files

--- myblog/urls.py
+++ myblog/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('account/', include('account.urls')),
     path('', include('personal.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
