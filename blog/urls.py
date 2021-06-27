f035005642cbff1d52b0166b88b1f501fac701d6
thanhlongnguyen140499 <thanhlongnguyen140499@gmail.com>
2021-06-27 17:47:49 +0700

    Update blog post

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ app_name = 'blog'
 urlpatterns = [
     path('create/', views.create_blog_view, name='create_blog'),
     path('detail/<slug>/', views.detail_blog_view, name='detail_blog'),
+    path('edit/<slug>/', views.edit_blog_view, name='edit_blog')
 ]
